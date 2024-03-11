8e7990b6d7133b12f25f4364a176b385d9c6cd69
Blake Frederick <bf@six12creative.com>
2024-03-10 21:56:23 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+I saw an Instagram reel that I found to be remarkably funny given that it was just boxes bouncing back and forth. So I decided to recreate that little animation (it's [here](https://www.instagram.com/p/CzINd8zsNxi)) for fun and lo and behold a very similar ending sequence just happens to take place.
+
+Thanks https://github.com/fuhton/piano-mp3 for the audio notes though I'd like to replace these sounds with something more musical.
+
+Needs fixing:
+- Page has to be clicked else no audio plays
--- bounce.js
+++ bounce.js
@@ -7,15 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 	let duration = 30000 // 30 seconds
 	let start = null
-	
+
 	const boxWidth = 100
 	const boxHeight = 100
 	const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 	const staggeredStart = 400
 	const bounceFlashDuration = 50
-	
+
 	class Box {
-	  constructor(y, colour, soundPath) {
+		constructor(y, colour, soundPath) {
 			this.x = 0
 			this.y = y
 			this.dx = 2 // speed seems reasonable
@@ -25,59 +25,58 @@ document.addEventListener('DOMContentLoaded', () => {
 			this.bounceTime = Date.now() + colours.indexOf(colour) * staggeredStart
 			this.lastBounce = -Infinity
 			this.sound = new Audio(soundPath)
-	  }
-  
-	  update() {
+		}
+
+		update() {
 			if (Date.now() > this.bounceTime) {
-		  if (this.x + this.dx > canvas.width - this.width || this.x + this.dx < 0) {
+				if (this.x + this.dx > canvas.width - this.width || this.x + this.dx < 0) {
 					this.dx = -this.dx // reverse!
 					this.lastBounce = Date.now()
 					this.sound.currentTime = 0
 					this.sound.play()
-		  }
-		  this.x += this.dx // keep going forward (or backward)
+				}
+				this.x += this.dx // keep going forward (or backward)
 			}
-	  }
-  
-	  draw() {
+		}
+
+		draw() {
 			ctx.fillStyle = this.colour
 			ctx.fillRect(this.x, this.y, this.width, this.height)
-  
+
 			// Flash effect on bounce
 			if (Date.now() - this.lastBounce <= bounceFlashDuration) {
-		  ctx.fillStyle = 'white'
-		  ctx.fillRect(this.dx > 0 ? this.x : this.x + this.width - 15, this.y, 15, this.height)
+				ctx.fillStyle = 'white'
+				ctx.fillRect(this.dx > 0 ? this.x : this.x + this.width - 15, this.y, 15, this.height)
 			}
-	  }
+		}
 	}
-	
+
 	const boxes = colours.map((colour, index) => {
-	  const soundPath = `C${index + 1}.mp3`
-	  return new Box(index * boxHeight, colour, soundPath)
+		const soundPath = `sounds/C${index + 1}.mp3`
+		return new Box(index * boxHeight, colour, soundPath)
 	})
-	
+
 	function animate(timestamp) {
 
 		// Canvas shrinking effect
-	  if (!start) start = timestamp
-	  let progress = timestamp - start
-	
-	  canvas.width = 800 - (progress / duration) * 800
-	  if (canvas.width < boxWidth) {
+		if (!start) start = timestamp
+		let progress = timestamp - start
+
+		canvas.width = 800 - (progress / duration) * 800
+		if (canvas.width < boxWidth) {
 			canvas.width = boxWidth // Do not shrink past the width of a box
-	  }
-  
-	  // Clear canvas and move boxes
-	  ctx.fillStyle = 'black'
-	  ctx.fillRect(0, 0, canvas.width, canvas.height)
-	  boxes.forEach(box => {
+		}
+
+		// Clear canvas and move boxes
+		ctx.fillStyle = 'black'
+		ctx.fillRect(0, 0, canvas.width, canvas.height)
+		boxes.forEach(box => {
 			box.update()
 			box.draw()
-	  })
-	
-	  requestAnimationFrame(animate)
+		})
+
+		requestAnimationFrame(animate)
 	}
-	
+
 	requestAnimationFrame(animate)
 })
-  
